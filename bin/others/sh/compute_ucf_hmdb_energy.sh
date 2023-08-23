b3386a60dd49956e08c14ea7a47f49d75f9ef0ff
simc <smh4477@naver.com>
2023-08-23 15:08:46 +0900

    working on test environments

--- bin/others/sh/compute_ucf_hmdb_energy.sh
+++
@@ -1,9 +0,0 @@
-for T in 0.1 0.2 0.4 0.8 1 2 4 8 16
-do
-    echo python -m bin.compute_ucf_hmdb_energy \
-        --ucf_fold 1 --hmdb_fold 1 --T ${T}
-    python -m bin.compute_ucf_hmdb_energy \
-        --ucf_fold 1 \
-        --hmdb_fold 1 \
-        --T ${T}
-done
